d464215d6537d642a86233d9e2f138fde5a683ea
musasyncs <rubato.cw@gmail.com>
2022-06-16 12:07:25 +0800

    Add dropdown menu for post in Home page

--- CatCafe/Controller/Tabs/HomeController.swift
+++ CatCafe/Controller/Tabs/HomeController.swift
@@ -8,9 +8,18 @@
 import UIKit
 import FirebaseAuth
 
-private let reuseIdentifier = "cell"
+private let reuseIdentifier = "dropDownCell"
 
 class HomeController: UICollectionViewController {
+    
+    let chatButton = UIButton(type: .system)
+    let notiButton = UIButton(type: .system)
+    let postButton = UIButton(type: .system)
+    lazy var chatBarButtonItem = UIBarButtonItem(customView: chatButton)
+    lazy var notiBarButtonItem = UIBarButtonItem(customView: notiButton)
+    lazy var postBarButtonItem = UIBarButtonItem(customView: postButton)
+    var showMenu = false
+    var dropTableView = UITableView(frame: .zero, style: .plain)
     
     // MARK: - Life Cycle
     
@@ -18,27 +27,25 @@ class HomeController: UICollectionViewController {
         super.viewDidLoad()
         setupRightNavItems()
         setupCollectionView()
+        setupDropDownMenu()
     }
     
     // MARK: - Helpers
     
     private func setupRightNavItems() {
-        
-        let chatButton = UIButton(type: .system)
         chatButton.setImage(UIImage(named: "send2")?.withRenderingMode(.alwaysOriginal), for: .normal)
         chatButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        chatButton.addTarget(self, action: #selector(gotoChatRoom), for: .touchUpInside)
         
-        let notificationButton = UIButton(type: .system)
-        notificationButton.setImage(UIImage(named: "like_unselected")?.withRenderingMode(.alwaysOriginal), for: .normal)
-        notificationButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        notiButton.setImage(UIImage(named: "like_unselected")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        notiButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        notiButton.addTarget(self, action: #selector(gotoNotificationPage), for: .touchUpInside)
         
-        let postButton = UIButton(type: .system)
         postButton.setImage(UIImage(named: "plus_unselected")?.withRenderingMode(.alwaysOriginal), for: .normal)
         postButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        postButton.addTarget(self, action: #selector(handleDropDownMenu), for: .touchUpInside)
         
-        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: chatButton),
-                                              UIBarButtonItem(customView: notificationButton),
-                                              UIBarButtonItem(customView: postButton)]
+        navigationItem.rightBarButtonItems = [chatBarButtonItem, notiBarButtonItem, postBarButtonItem]
     }
     
     func setupCollectionView() {
@@ -46,8 +53,72 @@ class HomeController: UICollectionViewController {
         collectionView.backgroundColor = .white
     }
     
+    func setupDropDownMenu() {
+        dropTableView.register(DropDownCell.self, forCellReuseIdentifier: reuseIdentifier)
+        dropTableView.delegate = self
+        dropTableView.dataSource = self
+    
+        dropTableView.separatorStyle = .singleLine
+        dropTableView.isScrollEnabled = false
+        dropTableView.rowHeight = 40
+        dropTableView.backgroundColor = .clear
+        dropTableView.layer.cornerRadius = 4
+    
+        view.addSubview(dropTableView)
+        dropTableView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                                 right: view.rightAnchor, paddingRight: 20,
+                                 width: 110, height: 80)
+    }
+    
     // MARK: - Actions
     
+    @objc func gotoChatRoom() {
+        
+    }
+    
+    @objc func gotoNotificationPage() {
+        
+    }
+    
+    @objc func handleDropDownMenu() {
+        showMenu = !showMenu
+        let indexPaths = [IndexPath(row: 0, section: 0),
+                          IndexPath(row: 1, section: 0)]
+        if showMenu {
+            dropTableView.insertRows(at: indexPaths, with: .fade)
+        } else {
+            dropTableView.deleteRows(at: indexPaths, with: .fade)
+        }
+    }
+    
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource - Drop down menu
+
+extension HomeController: UITableViewDelegate, UITableViewDataSource {
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return showMenu ? 2 : 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: reuseIdentifier,
+            for: indexPath
+        ) as? DropDownCell else { return UITableViewCell() }
+                
+        if indexPath.row == 0 {
+            cell.titleLabel.text = "發佈"
+        } else {
+            cell.titleLabel.text = "限時動態"
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
 }
 
 // MARK: - UICollectionViewDataSource
